7fdc00b4a5914fc36faf8fb31a6eb786ffdc4426
meganroberta80 <meganroberta89@gmail.com>
2021-07-28 16:35:09 -0700

    Modified the JS so that the images are a slideshow that changes every 3 seconds.

--- js/app.js
+++ js/app.js
@@ -9,27 +9,29 @@ let pictures = []
 function handleSubmit(event) {
     event.preventDefault()
     fetch(url + searchEl.value + "+nsfw:no")
-    .then((response) => {
-        return response.json()
-    }) 
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            // Create the DOM elements we need
+            intervalPictures = setInterval(loopPictures, 3000)
+            // Handy variables
+            pictures = data.data.children
+        })
+}
 
-    .then((data) => {
-    // Create the DOM elements we need
+// letintervalPictures = null
+let index = 0
+
+function loopPictures() {
+    index++
+    console.log(pictures[index])
+    let title = pictures[index].data.title
+    let thumbnail = pictures[index].data.thumbnail
+
     let divEl = document.createElement('div')
     let titleEl = document.createElement('h2')
     let imgEl = document.createElement('img')
-     
-    // Handy variables
-    pictures = data.data.children
-    })
-}
-let index = 0
-
-function loopPictures(event) {
-
-console.log(pictures[index])
-    let title = pictures[index].data.title
-    let thumbnail = pictures[index].data.thumbnail
 
     // Fill the DOM elements with our data
     titleEl.textContent = title
